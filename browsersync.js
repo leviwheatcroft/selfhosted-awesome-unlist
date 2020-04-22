0c99545c83a5f1633b65212a399830d33f541784
Levi Wheatcroft <levi@wheatcroft.com.au>
2020-04-22 18:14:50 +0800

    browsersync metalsmith-markdown metalsmith-layouts

--- browsersync.js
+++ browsersync.js
@@ -1,30 +1,20 @@
-import browserSync from 'browser-sync'
+const browserSync = require('browser-sync')
 
 /**
  * ## browser-sync
  * There's no external config file, this is everything. Just a simple static
  * server but it will set mime types for files on which we've deleted the
  * extension.
- *
- * `build.js` uses browser-sync's http api to trigger a reload.
  */
 browserSync.create()
-.init({
-  open: false,
-  server: {
-    baseDir: 'build/site',
-    index: 'index.html',
-    serveStaticOptions: {
-      extensions: ['html']
-    }
-    // serveStaticOptions: {
-    //   // this is a bit hacky. If there's no `.` within the last 5 chars of a
-    //   // path, then set the header to `text/html`. Works nicely for me. YMMV.
-    //   setHeaders: (res, path, stat) => {
-    //     if (/\..{1,4}$/.exec(path)) return
-    //     res.setHeader('content-type', 'text/html')
-    //   }
-    // }
-  },
-  watch: true
-})
+  .init({
+    open: false,
+    server: {
+      baseDir: 'build',
+      index: 'index.html',
+      serveStaticOptions: {
+        extensions: ['html']
+      }
+    },
+    watch: true
+  })
--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 #!/usr/bin/env node
 const Metalsmith = require('metalsmith')
 const debug = require('debug')
+
+const markdown = require('metalsmith-markdown')
+const layouts = require('metalsmith-layouts')
+
+
 const config = require('./config')
 
 const dbg = debug('metalsmith-build')
@@ -11,6 +16,16 @@ metalsmith.source('projects')
 metalsmith.clean(false)
 metalsmith.destination('build')
 metalsmith.metadata(config.meta)
+
+metalsmith.use(markdown())
+metalsmith.use(layouts({
+  directory: 'layouts',
+  pattern: '**/*',
+  default: 'project.pug',
+  engineOptions: {
+    basedir: 'layouts'
+  }
+}))
 metalsmith.build((err) => {
   if (err)
     throw err
