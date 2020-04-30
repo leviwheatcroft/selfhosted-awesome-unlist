6dbe75f2f3711fe4ed9e38e24c4531b3643f50bc
Levi Wheatcroft <levi@wheatcroft.com.au>
2020-05-01 03:28:08 +0800

    webpack pug importer

--- webpack.config.js
+++ webpack.config.js
@@ -44,6 +44,10 @@ module.exports = {
             loader: 'css-loader' // translates CSS into CommonJS
           }
         ]
+      },
+      {
+        test: /\.pug$/,
+        use: 'pug-loader'
       }
     ]
   },
